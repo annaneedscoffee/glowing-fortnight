8a51d74475fcf150331f0aa99656fc89660272ee
annaneedscoffee <72985178+annaneedscoffee@users.noreply.github.com>
2021-04-10 21:39:13 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace DiagramDesigner
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
